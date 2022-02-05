f6d226a490714eaaed843b7ad3a063bc332d72cc
Nando Machado <icenando@gmail.com>
2022-02-05 11:49:10 +0000

    Moved classes to Services folder

---
+++ Services/text_extraction.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from pytesseract import pytesseract
+
+from Services.image_processor import Image_Processor
+
+@dataclass
+class Text_Extraction:
+    image_processor: Image_Processor
+    
+    def extract_text(self, clean_img: str) -> str:
+        captcha = self.image_processor.read_captcha(clean_img)
+        captcha.show()
+        return pytesseract.image_to_string(captcha, config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
--- captcha_solver.py
+++ captcha_solver.py
@@ -1,72 +1,20 @@
-from dataclasses import dataclass
-from numpy import asarray, mean, ndarray
-from PIL import Image, ImageOps
-from scipy import ndimage
+from PIL import Image
 from os import path
-import pytesseract
+
+from Services.image_processor import Image_Processor
+from Services.text_extraction import Text_Extraction
 
 ##################### TWEEK THESE #####################
 captcha_img = "in/3.jpeg"
 out_img = path.join("out", path.basename(captcha_img))
 avg_deviation = 35
 u_filter = 4
-#######################################################
-
-@dataclass
-class Image_Processor:
-    
-    def read_captcha(self, captcha_img: str) -> Image:
-        return Image.open(captcha_img)
-        
-    def convert_img_to_monochrome(self, captcha_img: Image) -> Image:
-        return ImageOps.grayscale(captcha_img)
-    
-    def convert_img_to_array(self, input_img: Image) -> ndarray:
-        return asarray(input_img)
-
-
-    def increase_constrast(self, in_file: ndarray) -> ndarray:
-        
-        avg = mean(in_file, axis=(0, 1))
-        
-        for i, row in enumerate(in_file):
-            for pixel, value in enumerate(row):
-                # If pixel value is higher than the average of the image, 
-                # transforms to white, otherwise black.
-                if value <= avg + avg_deviation:
-                    in_file[i][pixel] = 0
-                else:
-                    in_file[i][pixel] = 255
-                    
-        return in_file
-
-
-    def neighbour_comparison(self, in_file: ndarray) -> ndarray:
-        # Fills in central pixel with thhe same colour as its neighbouring pixels. 
-        # The number is neighbouring pixels to compare agains is defined by "u_filter"
-        return ndimage.uniform_filter(in_file, u_filter, mode="constant", cval=0)
-    
-    def invert_colours(self, img: Image) -> Image:
-        return ImageOps.invert(img)
-    
-    def save_image(self, out_file: Image) -> None:
-        out_file.save(out_img)
-        out_file.close()
-        
-
-@dataclass
-class Text_Extraction(Image_Processor):
-    
-    def extract_text(self, clean_img: Image) -> str:
-        captcha = self.read_captcha(clean_img)
-        captcha.show()
-        return pytesseract.image_to_string(captcha, config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
-    
+#######################################################    
 
 if __name__=="__main__":
     
-    image_processor = Image_Processor()
-    text_extractor = Text_Extraction()
+    image_processor = Image_Processor(out_img, avg_deviation, u_filter)
+    text_extractor = Text_Extraction(image_processor)
 
     in_file = image_processor.convert_img_to_array(
             image_processor.convert_img_to_monochrome(
