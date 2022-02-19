6560ef7450c51881660327b4c49a6fa77bc21cb6
Nando Machado <icenando@gmail.com>
2022-02-19 13:09:46 +0000

    Added try except, logging

--- Services/text_extraction.py
+++ Services/text_extraction.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 
+import Logger
 from pytesseract import pytesseract
 
 from Services.image_processor import Image_Processor
+
+log = Logger.get_logger(__name__)
 
 
 @dataclass
@@ -10,8 +13,13 @@ class Text_Extraction:
     image_processor: Image_Processor
 
     def extract_text(self, clean_img: str) -> str:
-        captcha = self.image_processor.read_captcha(clean_img)
-        captcha.show()
-        return pytesseract.image_to_string(
-            captcha, config="--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789"
-        )
+        log.info(f"Extracting text from {clean_img}")
+        try:
+            captcha = self.image_processor.read_captcha(clean_img)
+            captcha.show()
+            return pytesseract.image_to_string(
+                captcha, config="--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789"
+            )
+        except Exception as e:
+            log.error("An error ocurred when recognisint the text")
+            raise SystemExit(f"An error ocurred when recognisint the text: {e}")
