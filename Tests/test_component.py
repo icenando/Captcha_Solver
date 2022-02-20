413f4458aa62bd5f088e156759563e0f0482ef8b
Nando Machado <icenando@gmail.com>
2022-02-20 18:53:21 +0000

    Component test now uses test specific resources

--- Tests/test_component.py
+++ Tests/test_component.py
@@ -7,6 +7,6 @@ class TestCaptchaSolverComponent:
     
     # TODO: test all images in Resources/in recursively
     def test_component(self):
-        captcha_img_file = path.join("Resources", "in", "2646.jpeg")
+        captcha_img_file = path.join("Tests", "Test_resources", "2646.jpeg")
         expected_result = path.join(path.basename(captcha_img_file)).split('.')[0]
         assert(captcha_solver.main(captcha_img_file)==expected_result)
