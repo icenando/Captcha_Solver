0418a6fab37e630a20afa04302a6cf95c406b512
Nando Machado <icenando@gmail.com>
2022-02-20 09:19:53 +0000

    Added text_extraction unit tests

--- Tests/test_component.py
+++ Tests/test_component.py
@@ -5,6 +5,7 @@ import captcha_solver
 
 class TestCaptchaSolverComponent:
     
+    # TODO: test all images in Resources/in recursively
     def test_component(self):
         captcha_img_file = path.join("Resources", "in", "2646.jpeg")
         expected_result = path.join(path.basename(captcha_img_file)).split('.')[0] + '\n'
