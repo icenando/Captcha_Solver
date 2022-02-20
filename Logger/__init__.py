890b8b71382c25212006b51c18577fdc751b82ac
Nando Machado <icenando@gmail.com>
2022-02-20 11:57:33 +0000

    Changed log level to warn

--- Logger/__init__.py
+++ Logger/__init__.py
@@ -3,7 +3,7 @@ import logging
 
 def get_logger(name: str) -> logging:
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.WARN)
     logging.basicConfig(
         format="[%(levelname)s] - %(asctime)s - %(name)s - %(message)s",
         datefmt="%m/%d/%Y %I:%M:%S %p",
