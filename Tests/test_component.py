60b3ed0cb43c7604e7c978926ec7c778ab73cfb3
icenando <icenando@gmail.com>
2022-02-22 21:29:35 +0000

    Merging github-test to dev (#7)
    * Added tesseract to github actions yaml
    
    * Fixed ndarray assertion
    
    * out_img is now parameter to captcha solver

--- Tests/test_component.py
+++ Tests/test_component.py
@@ -11,6 +11,7 @@ class TestCaptchaSolverComponent:
     
     # TODO: test all images in Resources/in recursively
     def test_component(self):
-        captcha_img_file = path.join("Tests", "Test_resources", "2646.jpeg")
+        captcha_img_file = path.join("Tests", "Test_resources", "in", "2646.jpeg")
         expected_result = path.join(path.basename(captcha_img_file)).split('.')[0]
-        assert(captcha_solver.main(captcha_img_file)==expected_result)
+        out_img = path.join("Tests", "Test_resources", "out", path.basename(captcha_img_file))
+        assert(captcha_solver.main(captcha_img_file, out_img)==expected_result)
